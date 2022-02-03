624d9a86dd9ea4096ee2610b9212d6026dec3ad6
Tobias Stelzer <tobias.stelzer@yahoo.com>
2022-02-03 13:31:15 +0100

    Update leafs.py
    Changed Number of epochs to 5

--- samples/leafs/leafs.py
+++ samples/leafs/leafs.py
@@ -213,7 +213,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=5,
                 layers='heads')
 
 
