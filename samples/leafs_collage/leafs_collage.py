5dfc8c67dba8b0bf08ab2291c9d795a444f29e92
Tobias Stelzer <tobias.stelzer@yahoo.com>
2022-03-10 10:57:37 +0100

    Update leafs_collage.py

--- samples/leafs_collage/leafs_collage.py
+++ samples/leafs_collage/leafs_collage.py
@@ -268,7 +268,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=100,
+                epochs=40,
                 layers='3+')
 
 
