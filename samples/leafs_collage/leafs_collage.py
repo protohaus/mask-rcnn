66691fbd29219643cf99e08b52d6f0798136279d
Tobias Stelzer <tobias.stelzer@yahoo.com>
2022-03-11 15:46:14 +0100

    Changed loss weights added BatchInference

--- samples/leafs_collage/leafs_collage.py
+++ samples/leafs_collage/leafs_collage.py
@@ -128,7 +128,7 @@ class LeafsCollageConfig(Config):
     # Can be used for R-CNN training setup.
     LOSS_WEIGHTS = {
         "rpn_class_loss": 1.,
-        "rpn_bbox_loss": 1.,
+        "rpn_bbox_loss": .1,
         "mrcnn_class_loss": 1.,
         "mrcnn_bbox_loss": 1.,
         "mrcnn_mask_loss": 1.
