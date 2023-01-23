fac8d621ce7a8fa7695b6c1b7d6e50eec0898e30
TStelzer86 <tobias.stelzer@yahoo.com>
2023-01-23 09:58:43 +0100

    Changed Layers for Training to 4+

--- samples/leafs_collage/leafs_collage.py
+++ samples/leafs_collage/leafs_collage.py
@@ -340,8 +340,7 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=50,
-                layers='3+')
-
+                layers='4+')
 
 def color_splash(image, mask):
     """Apply color splash effect.
